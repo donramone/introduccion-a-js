da8cae2a1a3f0e7db067c8b25a0484d418745647
luigi <luigi_lipreri@hotmail.com>
2019-11-20 10:21:36 -0300

    #42 - Solucion Tarea 2 - Fizz Buz.

--- tareas/clase-4/tarea-clase-4-resuelta.js
+++ tareas/clase-4/tarea-clase-4-resuelta.js
@@ -2,3 +2,36 @@ for(i=3;i <=22;i+=3){
     console.log(i);
 }
 console.log("***************** Final de Tarea N°1 ********************");
+
+//fizzbuz Clasico - como lo resolvi al principio.
+/*
+for(i=1;i<=50;i++){
+    let palabra="";
+    if(i % 3 ===0){
+       console.log("Fizz");
+        // palabra = palabra + "Fizz";
+    }if (i % 5 ===0){
+        //palabra = palabra + "Buzz";
+        console.log("Buzz");
+    }
+    if (i % 3 === 0 && i % 5 ===0){
+        console.log("Fizz Buzz");
+    }else{
+        console.log(i);
+    }
+}
+*/
+
+fizzBuzz();
+function fizzBuzz(){
+    for(i=1;i<=50;i++){
+        let word="";
+        if(i % 3 ===0){
+            word = word + "Fizz ";
+        }if (i % 5 ===0){
+            word = word + "Buzz";
+        }
+    console.log(word || i);
+    }
+}
+console.log("***************** Final de Tarea N°2 ********************");
