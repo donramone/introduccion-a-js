4deca79ae78395dd2008e6f3fa25c291fddd3f49
luigi <luigi_lipreri@hotmail.com>
2019-11-20 09:42:31 -0300

    #31 - Solucion Tarea 1 - Saludo nombre ingresado.

--- tareas/clase-3/tarea-clase-3-resuelta.js
+++ tareas/clase-3/tarea-clase-3-resuelta.js
@@ -1,1 +1,18 @@
 
+let nombreIngresado = (prompt("Ingresá tu nombre: ") || '' ).toLowerCase();
+const NOMBRE_MIO = "luigi";
+const NOMBRE_PADRE ="Julio";
+
+if(nombreIngresado.trim().length!==0){
+
+    if(NOMBRE_MIO.toLowerCase()===nombreIngresado){
+        console.log("Hola, Tocayo! Yo también me llamo " + nombreIngresado);
+    }else if(NOMBRE_PADRE.toLowerCase()===nombreIngresado){
+        console.log("Hola " + nombreIngresado + " te llamás igual que mi padre.");
+    }else{
+        console.log("Hola " + nombreIngresado);
+    }
+}else{
+    console.log("Parece ser que no ingresaste tu nombre.");
+}
+console.log("***************** Final de Tarea N°1 ********************");
