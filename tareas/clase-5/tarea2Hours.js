f187e4c07f83cf0ef4ddbc8af49d2418da423dff
luigi <luigi_lipreri@hotmail.com>
2019-12-09 12:40:51 -0300

    #53 Solución calculo de la duración total de los videos de las clases

---
+++ tareas/clase-5/tarea2Hours.js
@@ -0,0 +1,60 @@
+
+// <strong> pre-creado el tiempo total de los videos.
+const $btnAdd = document.querySelector("#btn-add");
+const $btnClean = document.querySelector("#btn-clean");
+let totalHours = 0;
+let totalMinutes = 0;
+let totalSeconds = 0;
+
+$btnAdd.onclick = function(){
+    const idClass = document.querySelector("#text-number-class").value;
+    const hours = document.querySelector("#text-hours").value;
+    const minutes = document.querySelector("#text-minutes").value;
+    const seconds = document.querySelector("#text-seconds").value;
+    const line = "Clase " + idClass + ": " + hours + "H "+ minutes + "M " + seconds + "S ";
+    addNewTimeClass(line);
+    totalTimes(hours,minutes,seconds);
+
+    return false;
+}
+
+$btnClean.onclick = function(){
+  console.log("clean");
+  cleanText();
+}
+
+function totalTimes(h, m, s){
+  totalHours += Number(h);
+  totalMinutes += Number(m);
+  totalSeconds += Number(s);
+ 
+  var hours = Math.floor(totalMinutes / 60 + totalHours);
+  console.log("horas de minutos: " + hours);
+  
+  var minutesRemainder = Math.floor(totalMinutes % 60);
+  console.log("restos minutos: " + minutesRemainder);
+
+  var minutes = Math.floor(totalSeconds / 60 + minutesRemainder);
+  console.log("minutos de segundo : " + minutes);
+
+  var secondsRemainder = Math.floor(totalSeconds % 60);
+  console.log("restos segundos: " + secondsRemainder);
+
+  document.querySelector('#result-times').textContent="Total: " + hours + "H. " + minutes + "M. " + secondsRemainder + "S.";
+}
+
+function addNewTimeClass(line){
+  var element = document.querySelector('#classes-times');
+  var pElem = document.createElement('p');
+  pElem.className = "pNewLineHour";
+  pElem.innerHTML = line;
+  element.appendChild(pElem);
+}
+
+function cleanText(){
+  totalHours = 0;
+  totalMinutes = 0;
+  totalSeconds = 0;
+  document.querySelector('#classes-times').textContent= "" ;
+  document.querySelector('#result-times').textContent= "";
+}
